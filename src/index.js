d6d580fdac81413b4b2eb1f534879feb0b618597
2kevink <kevink2@yandex.ru>
2018-09-14 03:13:40 +0300

    '7'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var k = 5;
+  var s = 0;
+  do{
+    s+=Math.floor(number/k);
+    number=Math.floor(number/k);
+
+  }while(number!=0)
+  return s;
 }
